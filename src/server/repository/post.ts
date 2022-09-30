79f60d7d81a0ac0bfc614c58d2434296c5567871
arganaphangquestian <arganaphangquestian@gmail.com>
2022-10-01 02:57:21 +0700

    create: repository

---
+++ src/server/repository/post.ts
@@ -0,0 +1,27 @@
+const GetPostsByType = (type: string) => {
+  return [];
+};
+
+const GetPostsByID = (id: string) => {
+  return null;
+};
+
+const CreatePost = (title: string, content: string, type: string) => {
+  return null;
+};
+
+const UpdatePostByID = (id: string, title: string, content: string) => {
+  return null;
+};
+
+const DeletePostByID = (id: string) => {
+  return null;
+};
+
+export {
+  GetPostsByType,
+  GetPostsByID,
+  CreatePost,
+  UpdatePostByID,
+  DeletePostByID,
+};
---
+++ src/server/repository/user.ts
@@ -0,0 +1,9 @@
+const GetUserByEmail = (email: string) => {
+  return null;
+};
+
+const GetUserByID = (id: string) => {
+  return null;
+};
+
+export { GetUserByEmail, GetUserByID };
